aae49d88658d8a5d9080257f437a9f67dbfe9a0d
Siddharth Kulkarni <10siddharthk@gmail.com>
2021-05-03 00:12:00 +0530

    implement jwt

--- land-records-backend/app.js
+++ land-records-backend/app.js
@@ -1,83 +1,96 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 app.use(express.json());
 const port = 3000;
 
 const uri = "mongodb://localhost:27017/land-records";
-const client = new MongoClient(uri);
+let client = new MongoClient(uri, { useUnifiedTopology: true });
+client.connect();
+let collection = client.db("land-records").collection("lands");
 
-app.get("/api/lands", async (req, res) => {
-    await client.connect();
-    try {
-        await client
-            .db("land-records")
-            .collection("lands")
-            .find({})
-            .toArray((err, results) => {
-                if (err) throw err;
-                res.json({ data: results });
-            });
-    } catch (err) {
-        console.log(err);
-    } finally {
-        client.close();
+app.post("/api/login", async (req, res) => {
+    if (req.body.username == "admin" && req.body.password == "admin@123") {
+        const token = generateAccessToken({
+            user: { username: req.body.username },
+        });
+        res.json({ token: token });
+    } else {
+        res.sendStatus(403);
     }
 });
 
-app.post("/api/lands", async (req, res) => {
-    await client.connect();
+app.get("/api/lands", authenticateToken, async (req, res) => {
+    // console.log("reached here");
     try {
-        // console.log(req);
-        await client
-            .db("land-records")
-            .collection("lands")
-            .insertOne(req.body.land);
-        res.json({ status: "success", land: req.body.land });
+        await collection.find({}).toArray((err, results) => {
+            if (err) throw err;
+            res.json({ data: results });
+        });
     } catch (err) {
-        console.log(err);
-    } finally {
-        client.close();
+        // console.log(err);
     }
 });
 
-app.patch("/api/lands", async (req, res) => {
-    await client.connect();
+app.post("/api/lands", authenticateToken, async (req, res) => {
     try {
-        // console.log(req);
-        await client
-            .db("land-records")
-            .collection("lands")
-            .updateOne(
-                { id: req.body.land.id },
-                { $set: { name: req.body.land.name } }
-            );
+        console.log(req);
+        await collection.insertOne(req.body.land);
         res.json({ status: "success", land: req.body.land });
     } catch (err) {
-        console.log(err);
+        // console.log(err);
+    }
+});
+
+app.patch("/api/lands", authenticateToken, async (req, res) => {
+    try {
+        console.log(req);
+        await collection.updateOne(
+            { id: req.body.land.id },
+            { $set: { name: req.body.land.name } }
+        );
+        res.json({ status: "success", land: req.body.land });
+    } catch (err) {
+        // console.log(err);
         res.sendStatus(422);
-    } finally {
-        client.close();
     }
 });
 
-app.delete("/api/lands", async (req, res) => {
-    await client.connect();
+app.delete("/api/lands", authenticateToken, async (req, res) => {
     try {
-        // console.log(req);
-        await client
-            .db("land-records")
-            .collection("lands")
-            .deleteOne({ id: req.body.land.id });
+        console.log(req);
+        await collection.deleteOne({ id: req.body.land.id });
         res.json({ status: "success", land: req.body.land });
     } catch (err) {
-        console.log(err);
+        // console.log(err);
         res.sendStatus(422);
-    } finally {
-        client.close();
     }
 });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+app.listen(port, async () => {
+    // console.log(`App listening at http://localhost:${port}`);
 });
+
+function generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_SECRET);
+}
+
+async function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) return res.sendStatus(401);
+    await jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        console.log(err);
+        if (err) {
+            // console.log("error");
+            return res.sendStatus(403);
+        }
+        // console.log("here");
+        req.user = user;
+        next();
+    });
+}
